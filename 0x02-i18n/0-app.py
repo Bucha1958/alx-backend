ee018cee747b8826f412114cc60f63101a71717e
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 00:41:25 -0700

    Add

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,12 +8,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.routes("/")
+
+@app.route("/")
 def index():
     """
     function that render template
     """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
