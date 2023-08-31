36bb2952ccbceef98c94b1ce48a347a059c7aa54
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 00:50:47 -0700

    task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,12 +9,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """
     function that render template
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
