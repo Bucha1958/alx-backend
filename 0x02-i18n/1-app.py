6c1c56317c791b9161c241c10ed027bec7824bd7
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 01:13:46 -0700

    task 2

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,10 +6,12 @@ My flask app file
 
 from flask import Flask, render_template
 from flask_babel import Babel
+from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
+app.config.from_object(Config)
 
 
 @app.route("/", strict_slashes=False)
@@ -17,7 +19,7 @@ def index():
     """
         function that render template
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""config module"""
+
+
+class Config:
+    """
+        config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
