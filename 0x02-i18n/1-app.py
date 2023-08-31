477392796530c186a0bb72e0fc088fb128db070a
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 01:21:10 -0700

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,11 +6,21 @@ My flask app file
 
 from flask import Flask, render_template
 from flask_babel import Babel
-from config import Config
 
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+class Config:
+    """
+        config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app.config.from_object(Config)
 
 
