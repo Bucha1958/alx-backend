19ca40e4e733331d6bb3c52f8ed4147338483bc9
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 01:35:01 -0700

    task 3

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+My flask app file
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+        config class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """
+        determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+        function that render template
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
